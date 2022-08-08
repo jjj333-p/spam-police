1950af00105b3f9a9d10466ed52e5726a5ec6dfd
jjj333_p <jjj333.p.1325@gmail.com>
2022-08-07 16:09:28 -1000

    started adding the ability to write to banlists. not for public use yet.

--- modules/message.js
+++ modules/message.js
@@ -195,6 +195,26 @@ class message {
 
             })
 
+        } else if (scannableContent.startsWith("+banlist")){
+
+            //parce the room that the cmd is referring to
+            let banlist = event["content"]["body"].split(" ")[1]
+
+            //use here so people dont have to type the room alias on an asinine client
+            if (banlist.toLowerCase() == "here") banlist = roomId
+
+            //resolve alias to an id for easier use
+            client.resolveRoom(banlist).then(banlistid=> {
+
+                //make banlist rule
+                client.sendStateEvent(banlistid, "m.policy.rule.user", ("rule:" + scannableContent.split(" ")[2]), {
+                    "entity": scannableContent.split(" ")[2],
+                    "reason": "impersonation",
+                    "recommendation": "org.matrix.mjolnir.ban"
+                },)
+
+            }).catch(err => client.sendNotice(roomId, "🍃 | I unfortunately ran into the following error while trying to run that command\n" + err))
+
         }
 
         // client.sendNotice(roomId, String(await client.userHasPowerLevelForAction(event["sender"], roomId, "kick")))

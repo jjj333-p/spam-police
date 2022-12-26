37044ceebb78b3893ce6ca958cc418cd4a6abdbb
jjj333_p <jjj333.p.1325@gmail.com>
2022-12-25 18:59:02 -1000

    [untested] add blacklist functionality to leave command

--- modules/message.js
+++ modules/message.js
@@ -169,7 +169,66 @@ class message {
             //verify is sent by an admin
             if ( this.authorizedUsers.some(u => u == event["sender"]) ){
 
-                client.leaveRoom(roomId)
+                //parce out the possible room id
+                let leaveRoom = event["content"]["body"].split(" ")[1]
+
+                //"+leave" as well as a space afterwards
+                let substringStart = 7
+
+                //if has the characters required for a room id or alias
+                if ((leaveRoom.includes("#") || leaveRoom.includes("!")) && leaveRoom.includes(":") && leaveRoom.includes(".")){
+
+                    //evaluate if its a valid alias
+                    client.resolveRoom(leaveRoom).then(async leaveroomid => {
+
+                        //add room id or alias to start the reason at the right part of the string
+                        substringStart = substringStart + leaveRoom.length + 1
+
+                        //parce out the reason
+                        let reason = event["content"]["body"].substring(substringStart)
+
+                        //add room to blacklist
+                        blacklist.add(leaveroomid, reason)
+
+                        //let the room know why the bot is leaving
+                        client.sendHtmlNotice(leaveroomid, "Leaving room for reason <code>" + reason + "</code>.")
+                            .finally(() => { //doesnt matter if unable to send to the room
+
+                                //attempt to leave the room
+                                client.leaveRoom(leaveroomid).then(() => {
+
+                                    //success message
+                                    client.sendHtmlNotice(roomId, "✅ | left room <code>" + leaveroomid + "</code> with reason <code>" + reason + "</code>.")
+
+                                }).catch(err => {
+
+                                    //error message
+                                    client.sendHtmlNotice(roomId, "❌ | I ran into the following error leaving the room: <code>" + err + "</code>")
+
+                                })
+
+                            })
+
+                    }).catch(err => {
+
+                        //throw error about invalid alias
+                        client.sendHtmlNotice(roomId, "❌ | I ran into the following error while trying to resolve that room ID:<blockquote>" + err.message + "</blockquote>")
+        
+                    })
+
+                //if cant possibly be a room alias, leave *this* room
+                } else {
+
+                    //parce out reason
+                    let reason = event["content"]["body"].substring(substringStart)
+
+                    //add to blacklist
+                    blacklist.add(roomId, reason)
+
+                    //leave room
+                    client.leaveRoom(roomId)
+
+                }
 
             } else {
 

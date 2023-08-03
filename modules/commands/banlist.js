db74ab19be4fd7a474dc668fafffda57a386d7cc
jjj333_p <jjj333.p.1325@gmail.com>
2023-08-03 11:19:20 -1000

    add the ability to remove banlist entry

--- modules/commands/banlist.js
+++ modules/commands/banlist.js
@@ -4,7 +4,7 @@ class Banlist {
 
     async run ({client, roomId, event, blacklist}, {scannableContent,offset, contentByWords}){
         //parce the room that the cmd is referring to
-        let banlist = contentByWords[1+offset]
+        let banlist = contentByWords[2+offset]
 
         //use here so people dont have to type the room alias on an asinine client
         if (banlist.toLowerCase() == "here") banlist = roomId
@@ -13,11 +13,11 @@ class Banlist {
         client.resolveRoom(banlist).then(banlistid=> {
 
             //account for all the chars when spit by spaces
-            let reasonStart = offset+2+1
+            let reasonStart = offset+3+1
 
             //for every word before the reason starts, count the length of that word
             //and add it to the offset
-            for(let i=0; i < (offset+2+1); i++){
+            for(let i=0; i < (offset+3+1); i++){
 
                 reasonStart += contentByWords[i].length
 
@@ -27,16 +27,35 @@ class Banlist {
             let reason = event["content"]["body"].substring(reasonStart)
 
             //parce out banned user
-            let bannedUser = contentByWords[2+offset]
+            let bannedUser = contentByWords[3+offset]
 
-            //make banlist rule
-            client.sendStateEvent(banlistid, "m.policy.rule.user", ("rule:" + bannedUser), {
-                "entity": bannedUser,
-                "reason": reason,
-                "recommendation": "org.matrix.mjolnir.ban"
-            },)
-                .catch(err => client.sendHtmlNotice(roomId, "<p>🍃 | I unfortunately ran into the following error while trying to add that to the banlist:\n</p><code>" + err+ "</code>"))
+            let action = contentByWords[1+offset].toLowerCase()
 
+            if (action == "add"){
+
+                //make banlist rule
+                client.sendStateEvent(banlistid, "m.policy.rule.user", ("rule:" + bannedUser), {
+                    "entity": bannedUser,
+                    "reason": reason,
+                    "recommendation": "org.matrix.mjolnir.ban"
+                },)
+                    .then(() => client.sendNotice(roomId, "✅ | Successfully set ban recomendation."))
+                    .catch(err => client.sendHtmlNotice(roomId, "<p>🍃 | I unfortunately ran into the following error while trying to add that to the banlist:\n</p><code>" + err+ "</code>"))
+
+            } else if (action == "remove" || action == "delete"){
+
+                //make banlist rule
+                client.sendStateEvent(banlistid, "m.policy.rule.user", ("rule:" + bannedUser), {
+                    "reason": reason,
+                },)
+                    .then(() => client.sendNotice(roomId, "✅ | Successfully removed ban recomendation."))
+                    .catch(err => client.sendHtmlNotice(roomId, "<p>🍃 | I unfortunately ran into the following error while trying to remove that banlist rule:\n</p><code>" + err+ "</code>"))
+
+            } else {
+
+                client.sendHtmlNotice(roomId, "<p>❌ | Invalid action, <code>" + action + "</code> != <code>add</code> or <code>remove</code>/<code>delete</code>.</p>")
+
+            }
 
         }).catch(err => client.sendHtmlNotice(roomId, "<p>🍃 | I unfortunately ran into the following error while trying to resolve that room:\n</p><code>" + err+ "</code>"))
         

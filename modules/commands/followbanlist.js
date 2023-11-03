7e7e68bc1d4022cb873b1109748b31648792c189
jjj333_p <jjj333.p.1325@gmail.com>
2023-11-02 16:33:06 -1000

    add remove functionality from followbanlist

--- modules/commands/followbanlist.js
+++ modules/commands/followbanlist.js
@@ -4,6 +4,15 @@ class FollowBanList {
 
     async run ({client, roomId, event, mxid, blacklist}, {offset, contentByWords, config}){
 
+        //check if the command even has the required feilds
+        if (contentByWords.length != (2+offset)){
+
+            client.sendNotice(roomId, "❌ | Malformed command.")
+
+            return
+
+        }
+
         //make sure the user has ban permissions before adding banlist
         if ( ! await client.userHasPowerLevelForAction(event["sender"], roomId, "ban") ) {
 
@@ -38,88 +47,114 @@ class FollowBanList {
         }
 
         //grep out the room indicated by the user
-        let joinroom = event["content"]["body"].split(" ")[1+offset]
+        let joinroom = contentByWords[2+offset]
 
         //evaluate if its a valid alias
         client.resolveRoom(joinroom).then(async joinroomid => {
 
-            //check blacklist for a blacklisted reason
-            let blReason = blacklist.match(joinroomid)
+            if ( contentByWords[1+offset] == "add" ) {
 
-            //if there is a reason that means the room was blacklisted
-            if(blReason) {
+                //if already following banlist, there is nothing to do
+                if( currentBanlists.includes(joinroomid) ){
 
-                //send error
-                client.sendHtmlNotice(roomId, "❌ | The bot was blacklisted from this room for reason <code>" + blReason + "</code>.")
+                    client.sendNotice(roomId, "♻️ | Already following this banlist.")
 
-                //dont continue trying to join
-                return
+                    return
 
-            }
+                }
 
-            //if already following banlist, there is nothing to do
-            if( currentBanlists.includes(joinroomid) ){
+                //check blacklist for a blacklisted reason
+                let blReason = blacklist.match(joinroomid)
 
-                client.sendNotice(roomId, "♻️ | Already following this banlist.")
+                //if there is a reason that means the room was blacklisted
+                if(blReason) {
 
-                return
+                    //send error
+                    client.sendHtmlNotice(roomId, "❌ | The bot was blacklisted from this room for reason <code>" + blReason + "</code>.")
 
-            }
+                    //dont continue trying to join
+                    return
 
-            //if the bot is already joined to the banlist, no need to try to join
-            if((await client.getJoinedRooms()).includes(joinroomid)){
+                }
 
-                //add the new banlist to the banlists
-                currentBanlists.push(joinroomid)
+                //if the bot is already joined to the banlist, no need to try to join
+                if((await client.getJoinedRooms()).includes(joinroomid)){
 
-                //set the config
-                config.setConfig(roomId, "banlists", currentBanlists, err => {
+                    //add the new banlist to the banlists
+                    currentBanlists.push(joinroomid)
+
+                    //set the config
+                    config.setConfig(roomId, "banlists", currentBanlists, err => {
+
+                        client.sendNotice(roomId, err)
+
+                    })
+
+                    return;
+
+                }
+
+                //deduce possible servers with the required information to join into the room
+                let aliasServer = joinroom.split(":")[1]
+                let senderServer = event["sender"].split(":")[1]
+                let botServer = mxid.split(":")[1]
+
+                //try to join
+                client.joinRoom(joinroomid, [aliasServer, senderServer, botServer, "matrix.org"]).then(() => {
+
+                    //greeting message
+                    let greeting = "Greetings! I am brought here by " + event["sender"] + ", bot by @jjj333:pain.agency (pls dm for questions). " + 
+                    "I am joining this room purely to read from the banlist, and as such will default to muted mode, however this can be changed with [prefix]mute. " + 
+                    "For more information please visit https://github.com/jjj333-p/spam-police"
+
+                    //add the new banlist to the banlists
+                    currentBanlists.push(joinroomid)
+
+                    //set the config
+                    config.setConfig(roomId, "banlists", currentBanlists, err => {
+
+                        client.sendNotice(roomId, err)
+
+                    })
+
+                    //try to send the greeting
+                    client.sendNotice(joinroomid, greeting).finally(() => {
+
+                        //confirm joined and can send messages
+                        client.sendNotice(roomId, "✅ | successfully joined banlist!")
+
+                    }).catch(err => {})
+
+                }).catch(err => {
+
+                    //throw error about joining room
+                    client.sendHtmlNotice(roomId, "❌ | I ran into the following error while trying to join that room:<blockquote>"  + JSON.stringify(err.body, null, 2) + "</blockquote>")
+
+                })
+
+
+            } else if ( contentByWords[1+offset] == "remove" ) {
+
+                //if not currently subscribed, there is nothing to remove
+                if ( !currentBanlists.some(b => b == joinroomid) ) {
+
+                    client.sendHtmlNotice(roomId, ("Not currently subscribed to any banlist with the RoomID <blacklist>" + joinroomid + "</blacklist>."))
+
+                    return
+
+                }
+
+                config.setConfig(roomId, "banlists", currentBanlists.filter(b => b != joinroomid), err => {
 
                     client.sendNotice(roomId, err)
 
                 })
 
-                return;
+            } else {
+
+                client.sendHtmlNotice(roomId, ("❌ | Unknown operation <blockquote>" + contentByWords[1+offset] + "</blockquote>, expected <blockquote>add</blockquote> or <blockquote>remove</blackquote>."))
 
             }
-
-            //deduce possible servers with the required information to join into the room
-            let aliasServer = joinroom.split(":")[1]
-            let senderServer = event["sender"].split(":")[1]
-            let botServer = mxid.split(":")[1]
-
-            //try to join
-            client.joinRoom(joinroomid, [aliasServer, senderServer, botServer, "matrix.org"]).then(() => {
-
-                //greeting message
-                let greeting = "Greetings! I am brought here by " + event["sender"] + ", bot by @jjj333:pain.agency (pls dm for questions). " + 
-                "I am joining this room purely to read from the banlist, and as such will default to muted mode, however this can be changed with [prefix]mute. " + 
-                "For more information please visit https://github.com/jjj333-p/spam-police"
-
-                //add the new banlist to the banlists
-                currentBanlists.push(joinroomid)
-
-                //set the config
-                config.setConfig(roomId, "banlists", currentBanlists, err => {
-
-                    client.sendNotice(roomId, err)
-
-                })
-
-                //try to send the greeting
-                client.sendNotice(joinroomid, greeting).finally(() => {
-
-                    //confirm joined and can send messages
-                    client.sendNotice(roomId, "✅ | successfully joined banlist!")
-
-                }).catch(err => {})
-
-            }).catch(err => {
-
-                //throw error about joining room
-                client.sendHtmlNotice(roomId, "❌ | I ran into the following error while trying to join that room:<blockquote>"  + JSON.stringify(err.body, null, 2) + "</blockquote>")
-
-            })
 
         }).catch(err => {
 
@@ -128,7 +163,6 @@ class FollowBanList {
 
         })
         
-
     }
 
 }

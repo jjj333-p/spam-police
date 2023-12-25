b3fbddd7f9d480fd65929f2ca0d510cbab8dc67f
jjj333_p <jjj333.p.1325@gmail.com>
2023-12-24 16:57:11 -1000

    stupid debugging

--- modules/reaction.js
+++ modules/reaction.js
@@ -8,6 +8,11 @@ class Reaction {
 
     async run ({client, roomId, event, mxid, scamBlEntries}) {
 
+        //should never happen but aparently it does
+        //https://matrix.pain.agency/_matrix/media/v3/download/pain.agency/51cc6283f64310640f67daa84f284ae8e7a08a969bd2f7f57920a4d30aa83c00
+        if(!event["content"]["m.relates_to"])             return;
+        if(!event["content"]["m.relates_to"]["event_id"]) return;
+
         if(roomId == this.logRoom){
 
             //make sure the reaction was to a scam entry
@@ -15,7 +20,7 @@ class Reaction {
 
             let senderpl = (await client.getRoomStateEvent(this.logRoom, "m.room.power_levels", null))["users"][event["sender"]]
 
-            if((senderpl === undefined) || (enderpl < 10 )) {return}
+            if((senderpl === undefined) || (senderpl < 10 )) {return}
 
             //if its a checkmark, run the confirm scam 
             if(event["content"]["m.relates_to"]["key"].includes("✅")){ scamBlEntries.get(event["content"]["m.relates_to"]["event_id"]).confirmScam(event["event_id"]) }

934c8d21351224f1e3f551172be6e4dbf651c3b7
jjj333_p <jjj333.p.1325@gmail.com>
2024-02-28 20:14:51 -1000

    add child and parent to banlist handling

--- modules/commands/banlist.js
+++ modules/commands/banlist.js
@@ -4,80 +4,121 @@ class Banlist {
 
     async run ({client, roomId, event}, {offset, contentByWords}){
 
-        //parce the room that the cmd is referring to
-        let banlist = contentByWords[2+offset]
+        try{
 
-        if (!banlist){
+            //parce the room that the cmd is referring to
+            let banlist = contentByWords[2+offset]
 
-            client.sendNotice(roomId, "❌ | invalid usage.")
+            if (!banlist){
 
-            return
+                client.sendNotice(roomId, "❌ | invalid usage.")
 
-        }
-
-        //use here so people dont have to type the room alias on an asinine client
-        if (banlist.toLowerCase() == "here") banlist = roomId
-
-
-        //resolve alias to an id for easier use
-        client.resolveRoom(banlist).then(async banlistid=> {
-
-            //make sure the user trying to write to the banlist can write to the banlist
-            if ( ! ( await client.userHasPowerLevelForAction(event["sender"], banlistid, "ban") || await client.userHasPowerLevelFor(event["sender"], banlistid, "m.policy.rule.user", true)) ) {
-
-                client.sendNotice(roomId, "❌ | You don't have sufficent permission in the banlist room.")
-                
                 return
+
+            }
+
+            //use here so people dont have to type the room alias on an asinine client
+            if (banlist.toLowerCase() == "here") banlist = roomId
+
+            //fetch spam police config
+            let stateconfigevent = (await client.getRoomState(roomId)).filter(event => event.type == "agency.pain.anti-scam.config")[0]
+            if (stateconfigevent) 
+                var stateconfig = stateconfigevent["content"]
             
-            }
+            //get room's children
+            if(stateconfig)
+                var children = stateconfig.children
+            
+            if (children && children[banlist])
+                banlist = children[banlist]
 
-            //account for all the chars when spit by spaces
-            let reasonStart = offset+3+1
 
-            //for every word before the reason starts, count the length of that word
-            //and add it to the offset
-            for(let i=0; i < (offset+3+1); i++){
+            //resolve alias to an id for easier use
+            client.resolveRoom(banlist).then(async banlistid=> {
 
-                reasonStart += contentByWords[i].length
 
-            }
+                //fetch spam police config
+                let blconfigevent = (await client.getRoomState(banlistid)).filter(event => event.type == "agency.pain.anti-scam.config")[0]
+                if (blconfigevent) 
+                    var blconfig = blconfigevent["content"]
+                
+                //get bl's parent
+                if(blconfig && blconfig )
+                    var blParent = blconfig.parent
+            
 
-            //parce the reason using the offset
-            let reason = event["content"]["body"].substring(reasonStart)
+                //make sure the user trying to write to the banlist can write to the banlist
+                //if true nothing happens, if it gets to the end of the chain it 
 
-            //parce out banned user
-            let bannedUser = contentByWords[3+offset]
+                if (Object.values(children).some(c => c == banlist)
+                    &&
+                    blParent == roomId
+                ) {
+                    var anonymous = true
+                }
+                //check pl
+                else if ( await client.userHasPowerLevelForAction(event["sender"], banlistid, "ban") 
+                    ||
+                    await client.userHasPowerLevelFor(event["sender"], banlistid, "m.policy.rule.user", true)
+                ){
+                } else {
+                    
+                    client.sendNotice(roomId, "❌ | You don't have sufficent permission in the banlist room.")
+                    
+                    return
+                
+                }
 
-            let action = contentByWords[1+offset].toLowerCase()
+                //account for all the chars when spit by spaces
+                let reasonStart = offset+3+1
 
-            if (action == "add"){
+                //for every word before the reason starts, count the length of that word
+                //and add it to the offset
+                for(let i=0; i < (offset+3+1); i++){
 
-                //make banlist rule
-                client.sendStateEvent(banlistid, "m.policy.rule.user", ("rule:" + bannedUser), {
-                    "entity": bannedUser,
-                    "reason": reason,
-                    "recommendation": "org.matrix.mjolnir.ban"
-                },)
-                    .then(() => client.sendNotice(roomId, "✅ | Successfully set ban recommendation."))
-                    .catch(err => client.sendHtmlNotice(roomId, "<p>🍃 | I unfortunately ran into the following error while trying to add that to the banlist:\n</p><code>" + err+ "</code>"))
+                    reasonStart += contentByWords[i].length
 
-            } else if (action == "remove" || action == "delete"){
+                }
 
-                //make banlist rule
-                client.sendStateEvent(banlistid, "m.policy.rule.user", ("rule:" + bannedUser), {
-                    "reason": reason,
-                },)
-                    .then(() => client.sendNotice(roomId, "✅ | Successfully removed ban recommendation."))
-                    .catch(err => client.sendHtmlNotice(roomId, "<p>🍃 | I unfortunately ran into the following error while trying to remove that banlist rule:\n</p><code>" + err+ "</code>"))
+                //parce the reason using the offset
+                let reason = event["content"]["body"].substring(reasonStart)
 
-            } else {
+                //parce out banned user
+                let bannedUser = contentByWords[3+offset]
 
-                client.sendHtmlNotice(roomId, "<p>❌ | Invalid action, <code>" + action + "</code> != <code>add</code> or <code>remove</code>/<code>delete</code>.</p>")
+                let action = contentByWords[1+offset].toLowerCase()
 
-            }
+                if (action == "add"){
 
-        }).catch(err => client.sendHtmlNotice(roomId, "<p>🍃 | I unfortunately ran into the following error while trying to resolve that room:\n</p><code>" + err+ "</code>"))
-        
+                    //make banlist rule
+                    client.sendStateEvent(banlistid, "m.policy.rule.user", ("rule:" + bannedUser), {
+                        "entity": bannedUser,
+                        "reason": reason,
+                        "recommendation": "org.matrix.mjolnir.ban"
+                    },)
+                        .then(() => client.sendNotice(roomId, "✅ | Successfully set ban recommendation."))
+                        .catch(err => client.sendHtmlNotice(roomId, "<p>🍃 | I unfortunately ran into the following error while trying to add that to the banlist:\n</p><code>" + err+ "</code>"))
+
+                } else if (action == "remove" || action == "delete"){
+
+                    //make banlist rule
+                    client.sendStateEvent(banlistid, "m.policy.rule.user", ("rule:" + bannedUser), {
+                        "reason": reason,
+                    },)
+                        .then(() => client.sendNotice(roomId, "✅ | Successfully removed ban recommendation."))
+                        .catch(err => client.sendHtmlNotice(roomId, "<p>🍃 | I unfortunately ran into the following error while trying to remove that banlist rule:\n</p><code>" + err+ "</code>"))
+
+                } else {
+
+                    client.sendHtmlNotice(roomId, "<p>❌ | Invalid action, <code>" + action + "</code> != <code>add</code> or <code>remove</code>/<code>delete</code>.</p>")
+
+                }
+
+            }).catch(err => client.sendHtmlNotice(roomId, "<p>🍃 | I unfortunately ran into the following error while trying to resolve that room:\n</p><code>" + err+ "</code>"))
+            
+        //this might fail, just catch it and move on
+        } catch(e) { console.log(e) }
+
     }
 
 }

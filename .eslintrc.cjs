1f31c5f66fd080e91803ba7121aca669a2e4b944
gnuxie <Gnuxie@protonmail.com>
2024-05-15 11:19:49 +0100

    Fixes for eslitn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,30 @@
 //
 // SPDX-License-Identifier: CC0-1.0
 
-const { gnuxieEslint } = require("gnuxie-tsconfig");
-
 module.exports = {
-  ...gnuxieEslint
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    project: ['tsconfig.json']
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    // we implement a lot of interfaces that return promises with synchronous functions.
+    "require-await": "off",
+    "@typescript-eslint/require-await": "off",
+    // we need never because our code can be wrong!
+    "@typescript-eslint/restrict-template-expressions": ['error', { allowNever: true }],
+  },
 };

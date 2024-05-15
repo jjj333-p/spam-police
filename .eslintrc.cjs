c7be2cd638bb0d1243dbe8a319a833ef059dabbc
gnuxie <Gnuxie@protonmail.com>
2024-05-15 11:12:16 +0100

    Add typescript and prettier etc.

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 Gnuxie <Gnuxie@protonmail.com>
+//
+// SPDX-License-Identifier: CC0-1.0
+
+const { gnuxieEslint } = require("gnuxie-tsconfig");
+
+module.exports = {
+  ...gnuxieEslint
+};

539bef3081b6d70604e46ff789be8f659b77a006
jjj333_p <jjj333.p.1325@gmail.com>
2024-07-12 00:48:15 -1000

    copy over old banlist code
    https://github.com/jjj333-p/spam-police/blob/21024a91da8ca44532e5a2168cc8a317e5c69372/modules/banlistReader.js#L1

--- modules/banlist.js
+++ modules/banlist.js
@@ -14,7 +14,78 @@ class BanlistReader {
 		this.eventCatcher = eventCatcher;
 	}
 	getRulesForUserOnRoom(mxid, roomID) {
-		this.clients.stateManager.getState(roomID, (e) => {});
+		return this.clients.stateManager.getState(roomID, (e) => {
+			let matchMXID = mxid;
+
+			//if the ban was erased
+			if (!econtent) {
+				return false;
+			}
+
+			//parce out the mxid from the key
+			const potentialMXID = econtent?.entity;
+
+			//if mismatch, its invalid
+			//mothet sidev
+			if (
+				econtent.recommendation !== "org.matrix.mjolnir.ban" &&
+				econtent.recommendation !== "m.ban"
+			) {
+				return false;
+			}
+
+			//if exact match, it is match
+			if (potentialMXID === matchMXID) {
+				return true;
+			}
+
+			//if theres no wildcards, and not an exact match, its not a match
+			if (!potentialMXID.includes("*")) {
+				return false;
+			}
+
+			//split around the wildcards
+			const p = potentialMXID.split("*");
+
+			//check before first wildcard
+			const firstpart = p.shift();
+			if (!matchMXID.startsWith(firstpart)) {
+				return false;
+			}
+
+			//check after last wildcard
+			const lastpart = p.pop();
+			if (!matchMXID.endsWith(lastpart)) {
+				return false;
+			}
+
+			//parce off the evaluated start and end
+			matchMXID = matchMXID.substring(
+				firstpart.length,
+				matchMXID.length - lastpart.length,
+			);
+
+			//loop until a condition to return arises
+			while (true) {
+				//if there is no more parts to match and everything else around wildcards matched
+				//it is match
+				if (p.length < 1) {
+					return true;
+				}
+
+				//if one of the bits between wildcards doesnt exist, it cant be a match
+				if (!matchMXID.includes(p[0])) {
+					return false;
+				}
+
+				//match the remaining parts in order starting with the first one left to match, then removing everything
+				//before it, and going back through the process untill theres nothing left to match
+				//have to use a substring of the original in case there was multiple matches of the next item to match
+				const nexttomatch = p.shift();
+				const r = matchMXID.split(nexttomatch);
+				matchMXID = matchMXID.substring(nexttomatch.length + r[0]);
+			}
+		});
 	}
 	getRulesForUser(mxid) {
 		//get banlists config and return if there is none

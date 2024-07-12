7c4cb6d100dcc90f5bab24436182cdac55797bfd
jjj333_p <jjj333.p.1325@gmail.com>
2024-07-12 00:32:26 -1000

    initial banlist class

--- modules/banlist.js
+++ modules/banlist.js
@@ -8,3 +8,39 @@ A complete copy of this codebase as well as runtime instructions can be found at
 https://github.com/jjj333-p/spam-police/
 */
 
+class BanlistReader {
+	constructor(clients, eventCatcher) {
+		this.clients = clients;
+		this.eventCatcher = eventCatcher;
+	}
+	getRulesForUserOnRoom(mxid, roomID) {
+		this.clients.stateManager.getState(roomID, (e) => {});
+	}
+	getRulesForUser(mxid) {
+		//get banlists config and return if there is none
+		const config = this.clients.stateManager.getConfig(roomID);
+		const banlists = config?.banlists;
+		if (!banlists) return;
+
+		//store results
+		results = [];
+
+		//for each followed banlist
+		for (const shortCode in banlists) {
+			//get rules, and on each of them
+			for (const event of this.getRulesForUserOnRoom(
+				mxid,
+				banlists[shortCode],
+			)) {
+				//add them to results
+				results.push({
+					shortCode,
+					roomID: banlists[shortCode],
+					event,
+				});
+			}
+		}
+
+		return results;
+	}
+}
